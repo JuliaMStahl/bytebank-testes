3dbade320252c0d6a0eba71e9c0ecb3f179b4eec
Julia Stahl <juliastahl@alunos.utfpr.edu.br>
2022-08-09 17:45:07 -0300

    Class 3 - flow test* - updating Dashboard with mockContactDao

--- test/dashboard_widget_test.dart
+++ test/dashboard_widget_test.dart
@@ -3,22 +3,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'matchers.dart';
+import 'mocks.mocks.dart';
 
 void main() {
   testWidgets('Should display the main image when Dashboard is opened', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mockContactDao = MockContactDao();
+    await tester.pumpWidget(MaterialApp(home: Dashboard(contactDao: mockContactDao,)));
     final mainImage = find.byType(Image);
     expect(mainImage, findsOneWidget);
   });
 
   testWidgets('Should display the Transfer feature when Dashboard is opened', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mockContactDao = MockContactDao();
+    await tester.pumpWidget(MaterialApp(home: Dashboard(contactDao: mockContactDao,)));
     final transferFeatureItem = find.byWidgetPredicate((widget) => featureItemMatcher(widget, 'Transfer', Icons.monetization_on));
     expect(transferFeatureItem, findsOneWidget);
   });
 
   testWidgets('Should display the Trannsaction Feed feature when Dashboard is opened', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(home: Dashboard()));
+    final mockContactDao = MockContactDao();
+    await tester.pumpWidget(MaterialApp(home: Dashboard(contactDao: mockContactDao,)));
     final transactionFeedFeatureItem = find.byWidgetPredicate((widget) => featureItemMatcher(widget, 'Transaction Feed', Icons.description));
     expect(transactionFeedFeatureItem, findsOneWidget);
   });
